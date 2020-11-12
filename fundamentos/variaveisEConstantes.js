44e99dba6fe2f3f0a82ba687b592a89cac1f14ce
Rafael <ribeirorafaelmatheus@gmail.com>
2020-11-11 21:24:40 -0300

    first commit

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,17 @@
+var a = 3
+let b = 4
+
+var a = 30
+b = 40 // Há problemas em redeclarar variáveis let da forma let b = 40
+
+console.log(a,b)
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+const c = 5 // não é possivel mudanças pelo usuário.
+            // Variveis imutaveis.
+console.log(c)
+
